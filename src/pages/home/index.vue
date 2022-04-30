a559e80b2214cae6b93c2f1dd5dda821742f6984
qige <1067970043@qq.com>
2022-04-30 20:08:26 +0800

    拉icon更新

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,6 +2,7 @@
 	<view class="content">
 		<view>
 			<text>首页</text>
+			<uni-icons custom-prefix="iconfont" type="&#xe8ef;;" size="30"></uni-icons>
 		</view>
 	</view>
 </template>
@@ -22,5 +23,5 @@
 </script>
 
 <style>
-	
+	@import "@/static/iconfont.css"
 </style>
--- src/pages/photo/index.vue
+++ src/pages/photo/index.vue
@@ -1,7 +1,45 @@
 <template>
 	<view class="content">
-		<view>
-			<h1>Hello photo</h1>
+		<view style="margin-top: 60rpx; padding: 20rpx;">
+			<uni-row class="demo-uni-row" gutter=20>
+				<uni-col :span="20">
+					<uni-easyinput 
+					v-model="input" 
+					placeholder="请输入检索文本" />
+				</uni-col>
+				<uni-col :span="4">
+					<view class="demo-uni-col light"></view>
+				</uni-col>
+			</uni-row>
+			<uni-row class="demo-uni-row">
+				<uni-col :span="12">
+					<view class="demo-uni-col dark"></view>
+				</uni-col> 
+				<uni-col :span="12">
+					<view class="demo-uni-col light"></view>
+				</uni-col>
+			</uni-row>
+			<uni-row class="demo-uni-row">
+				<uni-col :span="12">
+					<view class="demo-uni-col dark"></view>
+				</uni-col>
+				<uni-col :span="12">
+					<view class="demo-uni-col light"></view>
+				</uni-col>
+			</uni-row>
+			<uni-row class="demo-uni-row">
+				<uni-col :span="24">
+					<button type="primary">搜索</button>
+				</uni-col>
+			</uni-row>
+		</view>
+		<view id='imgs'>
+			<img 
+			  style=" width: 32%;height: 130px;padding: 2px;"
+			  v-for="composingImg in composingImgs" 
+			  :src="composingImg.image_src"
+			  v-on:click="toPictureDetail(composingImg)"
+			  alt="无法显示图片">
 		</view>
 	</view>
 </template>
@@ -10,6 +48,111 @@
 	export default {
 		data() {
 			return {
+				input:'',
+				types: [{
+				  value: '裂缝',
+				  label: '裂缝'
+				}, {
+				  value: '脱落',
+				  label: '脱落'
+				}, {
+				  value: '露筋',
+				  label: '露筋'
+				}, {
+				  value: '渗水',
+				  label: '渗水'
+				}],
+				type: '',
+				levels: [{
+					value: 1 ,
+					label: '等级1'
+				},{
+					value: 2,
+					label: '等级2'
+				},{
+					value: 3,
+					label: '等级3'
+				},{
+					value: 4,
+					label: '等级4'
+				}],
+				level:'',
+				beginTime:'',
+				endTime:'',
+				composingImgs:[{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/2.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/3.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/10.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/13.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/2.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/10.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/13.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/3.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				},{
+					image_id:1,
+					image_typeid:1,
+					image_src:'static/composing/2.jpg',
+					image_createtime:'2022-11-01',
+					image_isdelete:1,
+					image_remarks:'无',
+					user_id:1
+				}],
+				
 			}
 		},
 		onLoad() {
@@ -22,4 +165,37 @@
 </script>
 
 <style>
+	.demo-uni-row {
+		margin-bottom: 10px;
+		/* QQ、字节小程序文档写有 :host，但实测不生效 */
+		/* 百度小程序没有 :host，需要设置block */
+		/* #ifdef MP-TOUTIAO || MP-QQ || MP-BAIDU */
+		display: block;
+		/* #endif */
+	}
+	
+	/* 支付宝小程序没有 demo-uni-row 层级 */
+	/* 微信小程序使用了虚拟化节点，没有 demo-uni-row 层级 */
+	/* #ifdef MP-ALIPAY || MP-WEIXIN */
+	/deep/ .uni-row {
+		margin-bottom: 10px;
+	}
+	/* #endif */
+	
+	.demo-uni-col {
+		height: 36px;
+		border-radius: 4px;
+	}
+	
+	.dark_deep {
+		background-color: #99a9bf;
+	}
+	
+	.dark {
+		background-color: #d3dce6;
+	}
+	
+	.light {
+		background-color: #e5e9f2;
+	}
 </style>
