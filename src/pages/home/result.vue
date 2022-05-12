000ce17b7e60283ad3ac90ebdf29ed76f68347a1
StarPlatinumDa <276308719@qq.com>
2022-05-12 10:38:39 +0800

    图像识别接口完成，后续再加加载动画效果

--- src/pages/home/result.vue
+++ src/pages/home/result.vue
@@ -1,76 +1,110 @@
 <template>
-	<view style="text-align: center;">
-		<img 
-		class = "image"
-		:src="this.imagesrc"  
-		alt=""
-		>
-		<view class="">
-			<text style="float: left; margin: 0 30rpx;">识别结果：</text>
-			<text>{{this.diseasetype}}</text>
-		</view>
-		<img
-		class = "image"
-		:src="this.imagesrc"  
-		alt=""
-		>
-		<uni-row 
-		class="demo-uni-row" 
-		:gutter="50"
-		style="margin: 190rpx 0 0 0;"
-		>
-			<uni-col :span="15" >
-				<button 
-				style="background-color: #4F89F1; color: white; font-size: 110%;"
-				@click="toconfirm"
-				>上传图片</button>
-			</uni-col>
-			<uni-col :span="9">
-				<button 
-				style="background-color: #F14F4F; color: white; font-size: 110%;"
-				@click="backtohome"
-				>返回</button>
-			</uni-col>
-		</uni-row>
-	</view>
+  <view style="text-align: center;">
+    <img
+        class="image"
+        :src="this.imagesrc"
+        alt=""
+    >
+    <view class="">
+      <text style="float: left; margin: 0 30rpx;">识别结果：</text>
+      <text>{{ this.diseasetype }}</text>
+    </view>
+    <img
+        class="image"
+        :src="this.predictImgUrl"
+        alt=""
+        @click="previewImage(this.predictImgUrl)"
+    >
+    <uni-row
+        class=" demo-uni-row"
+        :gutter="50"
+        style="margin: 190rpx 0 0 0;"
+    >
+      <uni-col :span="15">
+        <button
+            style="background-color: #4F89F1; color: white; font-size: 110%;"
+            @click="toconfirm"
+        >上传图片
+        </button>
+      </uni-col>
+      <uni-col :span="9">
+        <button
+            style="background-color: #F14F4F; color: white; font-size: 110%;"
+            @click="backtohome"
+        >返回
+        </button>
+      </uni-col>
+    </uni-row>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				imagesrc: 'static/composing/2.jpg',
-				diseasetype: '渗水'
-			}
-		},
-		onLoad(option) {
-			let n = option.imagesrc.length;
-			if(option.tratype == 1)
-				this.imagesrc = option.imagesrc;
-			else
-				this.imagesrc = option.imagesrc.substr(1, n-2);
-		},
-		methods: {
-			toconfirm() {
-				uni.navigateTo({
-					url:`/pages/home/confirm?imagesrc=${this.imagesrc}`
-				})
-			},
-			backtohome() {
-				uni.switchTab({
-				  url: '/pages/home/index'
-				})
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      imagesrc: 'static/composing/2.jpg',
+      predictImgUrl: '',
+      diseasetype: '渗水',
+      detectionLabel: ''
+    }
+  },
+  onLoad(option) {
+    let n = option.imagesrc.length;
+    if (option.tratype == 1)
+      this.imagesrc = option.imagesrc;
+    else
+      this.imagesrc = option.imagesrc.substr(1, n - 2);
+
+    this.preImage()
+  },
+  methods: {
+    toconfirm() {
+      uni.navigateTo({
+        url: `/pages/home/confirm?imagesrc=${this.imagesrc}`
+      })
+    },
+    backtohome() {
+      uni.switchTab({
+        url: '/pages/home/index'
+      })
+    },
+    //  图像识别模块
+    preImage() {
+      uni.uploadFile({
+        url: 'http://127.0.0.1:5000/swinimg',
+        header: {
+          'Context-Type': "multipart/form-data"
+        },
+        filePath: this.imagesrc,
+        name: "img",
+        success: (result => {
+          let data = JSON.parse(result.data)
+          this.detectionLabel = data.label
+          console.log(data.label)
+          this.predictImgUrl = 'data:image/jpeg;base64,' + data.img
+        })
+      })
+    },
+    previewImage(src) {
+      console.log('image')
+      console.log(src)
+      let array = []
+      array.push(src)
+      uni.previewImage({
+        urls: array,
+        current: array[0]
+      })
+    },
+  }
+}
 </script>
 
 <style>
-	.image{
-		width: 95%; 
-		height: 500rpx; 
-		margin: 30rpx 0; 
-		border-radius: 20rpx;
-	}
+.image {
+  width: 95%;
+  height: 500 rpx;
+  margin: 30 rpx 0;
+  border-radius: 20 rpx;
+}
 
 </style>
